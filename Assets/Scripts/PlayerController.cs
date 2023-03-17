2e4531bb6229bba8beecd2ec9aa13d4e167c26ab
Roi Martinez <rmartinezroque@danielcastelao.org>
2023-03-17 10:00:29 +0100

    updt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Transactions;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
